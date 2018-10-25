ad831c036beb6109313ca57838cc6d631eb5465c
Maxence Theme <matheme@z1r3p4.le-101.fr>
2018-10-25 14:59:25 +0200

    mise a jour du fillit.h

--- fillit.h
+++ fillit.h
@@ -1,6 +1,27 @@
-#ifndef FILLIT_H
-#define FILLIT_H
-# include "libft/includes/libft.h"
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   fillit.h                                         .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: matheme <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/10/25 11:50:42 by matheme      #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/25 14:58:47 by matheme     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
 
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include "libft/includes/libft.h"
+# include "get_next_line.h"
+
+typedef struct	p_list
+{
+	char		*str;
+	p_list		*next;
+	p_list		*prev;
+}				f_list;
 
 #endif
