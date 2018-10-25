b92a90096130c6e48019209f96a923cdff31fa15
Eric leo william Schneller <eschnell@student.le-101.fr>
2018-10-25 15:23:48 +0200

    gnl dans libft/

--- libft/get_next_line.h
+++ libft/get_next_line.h
@@ -3,22 +3,29 @@
 /*                                                              /             */
 /*   get_next_line.h                                  .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: matheme <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*   By: eschnell <eschnell@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2018/10/11 17:43:53 by matheme      #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/24 11:10:41 by matheme     ###    #+. /#+    ###.fr     */
+/*   Created: 2018/10/11 19:58:18 by eschnell     #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/24 15:48:22 by eschnell    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# define BUFF_SIZE 10
+# define BUFF_SIZE 1
 # include <sys/types.h>
 # include <sys/uio.h>
-# include <fcntl.h>
+# include <unistd.h>
 # include "libft/includes/libft.h"
 
+typedef	struct		s_gnline
+{
+	int				fd;
+	char			*tmp;
+	struct s_gnline	*next;
+}					t_gnline;
+
 int					get_next_line(const int fd, char **line);
+
 #endif
