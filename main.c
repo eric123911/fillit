83bedd30c72fcd80cb7a8279ac3956fc28e3cf1e
eric123911 <eric_schneller@hotmail.com>
2018-10-26 15:35:08 +0200

    modif sans importance

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 #include "fillit.h"
 
 f_list	*ft_read(char *av)
-{	
+{
 	int f;
 	f_list	*list;
 	f_list	*begin;
-	
+
 	f = open(av, O_RDONLY);
 	list = malloc(sizeof(f_list));
 	begin = list;
 	while (get_next_line(f, &list->str) > 0)
 	{
 		list->next = malloc(sizeof(f_list));
-		list->next->prev = list;
 		list = list->next;
 	}
 	list->next = NULL;
+	list->prev = NULL;
 	return (begin);
 }
 
