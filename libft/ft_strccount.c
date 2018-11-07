f67b4f8d6394fbde6401d4215bd3290142505a7a
Eric leo william Schneller <eschnell@student.le-101.fr>
2018-11-07 14:46:23 +0100

    norme

--- libft/ft_strccount.c
+++ libft/ft_strccount.c
@@ -6,22 +6,22 @@
 /*   By: matheme <matheme@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/25 16:02:34 by eschnell     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/29 14:51:42 by matheme     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/07 14:36:08 by eschnell    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t  ft_strccount(const char *s, char c)
+size_t	ft_strccount(const char *s, char c)
 {
-	size_t  i;
+	size_t	i;
 	size_t	j;
 
 	i = -1;
 	j = 0;
-    while (s[++i])
-        if (s[i] == c)
-            j++;
-    return (j);
+	while (s[++i])
+		if (s[i] == c)
+			j++;
+	return (j);
 }
