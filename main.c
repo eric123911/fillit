502ceeacc1059ed661c9bd6eb21707a2e4095019
Maxence Theme <matheme@student.le-101.fr>
2018-11-13 16:35:48 +0100

    optimisation + usage

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: matheme <matheme@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/12 13:18:47 by matheme      #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/13 15:50:57 by eschnell    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/13 16:13:50 by matheme     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -30,7 +30,7 @@ void	ft_put_tab(char **tab)
 	}
 }
 
-f_list	*ft_read(char *av)
+f_list	*ft_read(char *av, size_t *line)
 {
 	int		f;
 	f_list	*list;
@@ -42,6 +42,7 @@ f_list	*ft_read(char *av)
 	begin = list;
 	while (get_next_line(f, &list->str) > 0)
 	{
+		(*line)++;
 		list->next = malloc(sizeof(f_list));
 		list->next->prev = list;
 		list = list->next;
@@ -77,20 +78,25 @@ size_t	map_size(short d)
 	return ((res * res == (size_t)d) ? res : res + 1);
 }
 
-int		main(void)//int ac, char **av)
+int		main(int ac, char **av)
 {
 	f_list	*list;
 	char	**tab;
+	size_t  line;
 	size_t	size;
 
-	//printf("%zu\n", map_size(_____));
-
-	size = 2;
+	line = 1;
 	if (ac != 2)
-		return (1); 	// <========== FAT USAGE A METTER ICI
-	list = ft_read(av[1]);
+	{
+		ft_putstr("usage: ");
+		ft_putstr(av[0]);
+		ft_putendl(" file_name");
+		return (1);
+	}
+	list = ft_read(av[1], &line);
 	if (validate_file(list))
 		return (error());
+	size = map_size((line / 5) * 4);
 	tab = fillit(list, size, 'A', ft_tab_malloc(size));
 	ft_put_tab(tab);
 	return (0);
